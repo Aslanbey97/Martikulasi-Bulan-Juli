b34b7b586c75f0129a276b7c6899ec4ae5f1357b
Aslanbey97 <107825929+Aslanbey97@users.noreply.github.com>
2022-07-07 14:25:57 +0700

    blajar Python

---
+++ Hello.py
@@ -0,0 +1,11 @@
+# print("Hello world")
+# print("welcome to My Website")
+
+# num = eval(input("Masukkan sebuah bilangan: "))
+# print("Hasil kuadrat bilangan tersebut adalah", num * num)
+
+print("Berapakah nilai v jika x = 2, y = 4, z = 3")
+x = eval(input("x = "))
+y = eval(input("y = "))
+z = eval(input("z = "))
+print ("Nilai v = ", ((x + y)/z)-5)
