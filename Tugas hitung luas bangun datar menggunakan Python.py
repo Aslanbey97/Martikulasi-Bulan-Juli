99671316144a70c0968b8a273adad7fc1e53b5ab
Aslanbey97 <107825929+Aslanbey97@users.noreply.github.com>
2022-07-20 20:15:01 +0700

    Tugas hitung luas bangun datar menggunakan Python

---
+++ Tugas hitung luas bangun datar menggunakan Python.py
@@ -0,0 +1,59 @@
+nama = input(" Input your Name : ")
+nim = input(" Input your NIM : ")
+
+
+print ('--------------------------------------------')
+print ('_ _Welcome to Aslanbey_ _')
+print ('_ _With you NIM 948797_ _')
+print ('--------------------------------------------')
+
+
+# soal 1
+print("\nQuestion No.1\nDiketahui sisi = 6, Sebutkan Nama dan hitunglah Luas Bangun Datar tersebut!")
+bangDar1 = input("Nama Bangun Datar = ") 
+sisi1 = eval(input("Sisi : "))
+sisi2 = eval(input("Sisi : "))
+print("Bangun Datar tersebut adalah", bangDar1, " dan Luas Bangun Datar Tersebut adalah",(sisi1*sisi2))
+
+# soal 2
+print("\nQuestion No.2\nDiketahui alas = 7, tinggi = 8, hitunglah Luas Bangun Datar tersebut!")
+bangDar1 = input("Nama Bangun Datar = ") 
+Alas = eval(input("Alas : "))
+Tinggi = eval(input("Tinggi : "))
+print("Bangun Datar tersebut adalah", bangDar1, " dan Luas Bangun Datar Tersebut adalah",(Alas*Tinggi/2))
+
+# soal 3
+print("\nQuestion No.3\nDiketahui panjang = 4, Lebar = 9, Sebutkan Nama dan hitunglah Luas Bangun Datar tersebut!")
+bangDar1 = input("Nama Bangun Datar = ") 
+Panjang = eval(input("Panjang : "))
+Lebar = eval(input("Lebar : "))
+print("Bangun Datar tersebut adalah", bangDar1, " dan Luas Bangun Datar Tersebut adalah",(Panjang*Lebar))
+
+# soal 4
+print("\nQuestion No.4\nDiketahui Jari-jari = 56, Sebutkan Nama dan hitunglah Luas Bangun Datar tersebut!")
+bangDar1 = input("Nama Bangun Datar = ") 
+π = eval(input("π : "))
+r = eval(input("r : "))
+print("Bangun Datar tersebut adalah", bangDar1, " dan Luas Bangun Datar Tersebut adalah",(π*r*r))
+
+# soal 5
+print("\nQuestion No.5\nDiketahui I = 4, Diagonal II = 8, Sebutkan Nama dan hitunglah Luas Bangun Datar tersebut!")
+bangDar1 = input("Nama Bangun Datar = ") 
+DiagonalI = eval(input("Diagonal I : "))
+DiagonalII = eval(input("Diagonal II : "))
+print("Bangun Datar tersebut adalah", bangDar1, " dan Luas Bangun Datar Tersebut adalah",(1/2*DiagonalI*DiagonalII))
+
+# soal 6
+print("\nQuestion No.6\nDiketahui sisi AB dan CD sejajar, sisi AB = 4, sisi CD = 8, tinggi = 3, Sebutkan Nama dan hitunglah Luas Bangun Datar tersebut!")
+bangDar1 = input("Nama Bangun Datar = ") 
+sisi1 = eval(input("Sisi AB : "))
+sisi2 = eval(input("Sisi CD : "))
+Tinggi = eval(input("Tinggi : "))
+print("Bangun Datar tersebut adalah", bangDar1, " dan Luas Bangun Datar Tersebut adalah",(sisi1+sisi2*Tinggi/2+3))
+
+# soal 7
+print("\nQuestion No.7\nDiketahui alas = 9, tinggi = 7, Sebutkan Nama dan hitunglah Luas Bangun Datar tersebut!")
+bangDar1 = input("Nama Bangun Datar = ") 
+Alas = eval(input("Alas : "))
+Tinggi = eval(input("Tinggi : "))
+print("Bangun Datar tersebut adalah", bangDar1, " dan Luas Bangun Datar Tersebut adalah",(Alas*Tinggi))
